f851de57f51a67be2953db2ec0a52a9c94810a51
Jakub Abram <j.abram1@wp.pl>
2025-11-12 13:55:40 +0100

    Added get all blogs route

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const {Blog} = require("./models/blog")
 
 dotenv.config();
 
 const app = express()
+
+app.use(express.urlencoded({extended: true}));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
@@ -14,3 +17,15 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => {
         console.log(err)
     })
+
+app.get("/blogs", async(req, res) => {
+    try{
+        const blogs = await Blog.find();
+        res.send(blogs)
+    }catch{
+        res.status(500);
+        res.send("error");
+    }
+})
+
+
