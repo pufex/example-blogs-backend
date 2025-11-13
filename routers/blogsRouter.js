e026ea4aa2db25cb0368b8920e8f900a53afe7b9
Jakub Abram <j.abram1@wp.pl>
2025-11-13 18:32:03 +0100

    Created a blog router and moved it to another file

---
+++ routers/blogsRouter.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { Blog } = require("../models/blog");
+
+const blogsRouter = express.Router()
+
+blogsRouter.get("/blogs", async(req, res) => {
+    try{
+        const blogs = await Blog.find();
+        res.send(blogs)
+    }catch{
+        res.status(500);
+        res.send("error");
+    }
+})
+
+blogsRouter.get("/blogs/:id", async (req, res) => {
+    try{
+        const blog_id = req.params.id
+        const blog = await Blog.findById(blog_id)
+        if(!blog){
+            res.status(404)
+            res.send(null)
+        }else{
+            res.send(blog);
+        }
+    }catch{
+        res.status(500)
+        res.send("error")
+    }
+})
+
+blogsRouter.post("/blogs", async (req, res) => {
+    try{
+        const blog = await new Blog(req.body);
+        await blog.save();
+        res.send(blog);
+    }catch(err){
+        console.log(err)
+        console.log(body)
+        res.status(500)
+        res.send("error")
+    }
+})
+
+blogsRouter.delete("/blogs/:id", async (req, res) => {
+    try{
+        const blog_id = req.params.id
+        await Blog.findByIdAndDelete(blog_id);
+        res.send("OK")
+    }catch(err){
+        console.log(err)
+        res.status(500)
+        res.send("error")
+    }
+})
+
+module.exports = blogsRouter
